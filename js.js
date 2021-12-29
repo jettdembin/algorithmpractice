0da5ed08aa59c832231ce14bc0fc6f7191dcd360
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-12-29 07:37:33 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,67 @@
+// This file explores the use of Data Structures and Algorithms
+
+// STACK
+
+const letters = [];
+
+const word = "racecar";
+
+let rword = "";
+
+// put letters of word into stack
+for (let i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+//pop off the stack in reverse order
+for (let i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+if (rword === word) {
+  console.log(word + " is a palindrome.");
+} else {
+  console.log(word + " is not a palindrome.");
+}
+
+// Implementation with Constructor Function
+
+const Stack = function() {
+  this.count = 0;
+  this.storage = {};
+
+  // Adds a value onto the end of the stack
+  this.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  // Removes and returns the value at the end of the stack
+  this.pop = function() {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count --;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result
+  }
+
+  this.size = function() {
+    return this.count;
+  }
+
+  // Returns the value at the end of the stack
+  this.peek = function(value) {
+    return this.storage[this.count-1];
+  }
+}
+
+const myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
